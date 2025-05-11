aa2a2fafdd188d0666d02591a3872cd1e17c2119
MedicodiBiscotti <13235260+MedicodiBiscotti@users.noreply.github.com>
2025-05-11 19:25:30 +0200

    base64 encode

--- src/main/java/dk/kavv/uuideck/App.java
+++ src/main/java/dk/kavv/uuideck/App.java
@@ -1,14 +1,18 @@
 package dk.kavv.uuideck;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 import java.util.stream.IntStream;
 
 public class App {
     public static void main(String[] args) {
         List<Integer> deck = new ArrayList<>(IntStream.range(0, 52).boxed().toList());
         Collections.shuffle(deck);
-        System.out.println(deck);
+        byte[] bytes = new byte[deck.size()];
+        for (int i = 0; i < deck.size(); i++) {
+            bytes[i] = deck.get(i).byteValue();
+        }
+        System.out.println(Arrays.toString(bytes));
+        String encoded = Base64.getEncoder().encodeToString(bytes);
+        System.out.println(encoded);
     }
 }
