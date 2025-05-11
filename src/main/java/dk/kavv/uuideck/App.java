753d3afe39fd7c1e4f924ed3ca6fb2b2a5f33de0
MedicodiBiscotti <13235260+MedicodiBiscotti@users.noreply.github.com>
2025-05-11 20:19:22 +0200

    print as card

--- src/main/java/dk/kavv/uuideck/App.java
+++ src/main/java/dk/kavv/uuideck/App.java
@@ -12,7 +12,28 @@ public class App {
             bytes[i] = deck.get(i).byteValue();
         }
         System.out.println(Arrays.toString(bytes));
+        System.out.println(deck.stream().map(App::toCard).toList());
         String encoded = Base64.getEncoder().encodeToString(bytes);
         System.out.println(encoded);
+    }
+
+    public static String toCard(int b) {
+        int suit = b / 13;
+        int rank = b % 13;
+        String res = switch (rank) {
+            case 0 -> "A";
+            case 10 -> "J";
+            case 11 -> "Q";
+            case 12 -> "K";
+            default -> String.valueOf(rank + 1);
+        };
+        res += switch (suit) {
+            case 0 -> "S";
+            case 1 -> "C";
+            case 2 -> "D";
+            case 3 -> "H";
+            default -> throw new IllegalStateException("Unexpected value: " + suit);
+        };
+        return res;
     }
 }
