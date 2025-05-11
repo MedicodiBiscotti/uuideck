01e62d3a888fc07b7e5c4f5d558c8cb128e84a0f
MedicodiBiscotti <13235260+MedicodiBiscotti@users.noreply.github.com>
2025-05-11 15:40:59 +0200

    simple implementation

--- src/main/java/dk/kavv/uuideck/App.java
+++ src/main/java/dk/kavv/uuideck/App.java
@@ -1,10 +1,14 @@
 package dk.kavv.uuideck;
 
-/**
- * Hello world!
- */
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.IntStream;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        List<Integer> deck = new ArrayList<>(IntStream.range(0, 52).boxed().toList());
+        Collections.shuffle(deck);
+        System.out.println(deck);
     }
 }
