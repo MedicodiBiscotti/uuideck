3a1879eb8ddf939baef0567d1c0de8310ed10b4f
MedicodiBiscotti <13235260+MedicodiBiscotti@users.noreply.github.com>
2025-05-13 19:49:15 +0200

    refactor to methods

--- src/main/java/dk/kavv/uuideck/App.java
+++ src/main/java/dk/kavv/uuideck/App.java
@@ -1,23 +1,41 @@
 package dk.kavv.uuideck;
 
 import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class App {
     public static void main(String[] args) {
+        byte[] bytes = generateDeck(new Random());
+        System.out.println(Arrays.toString(bytes));
+        presentDeck(bytes);
+        String encoded = encode(bytes);
+        System.out.println(encoded);
+    }
+
+    public static byte[] generateDeck(Random r) {
         List<Integer> deck = new ArrayList<>(IntStream.range(0, 52).boxed().toList());
-        Collections.shuffle(deck);
+        Collections.shuffle(deck, r);
         byte[] bytes = new byte[deck.size()];
         for (int i = 0; i < deck.size(); i++) {
             bytes[i] = deck.get(i).byteValue();
         }
-        System.out.println(Arrays.toString(bytes));
-        System.out.println(deck.stream().map(App::toCard).toList());
-        String encoded = Base64.getEncoder().encodeToString(bytes);
-        System.out.println(encoded);
+        return bytes;
     }
 
-    public static String toCard(int b) {
+    public static String encode(byte[] bytes) {
+        return Base64.getEncoder().encodeToString(bytes);
+    }
+
+    public static void presentDeck(byte[] bytes) {
+        // Alternatively, StringJoiner and for loop.
+        System.out.println(
+                IntStream.range(0, bytes.length)
+                        .mapToObj(i -> toCard(bytes[i]))
+                        .collect(Collectors.joining(", ")));
+    }
+
+    public static String toCard(byte b) {
         int suit = b / 13;
         int rank = b % 13;
         String res = switch (rank) {
