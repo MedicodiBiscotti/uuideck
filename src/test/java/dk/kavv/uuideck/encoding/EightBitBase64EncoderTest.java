6d1a724924a137d9f2f4200deaac4e0919c23d5f
MedicodiBiscotti <13235260+MedicodiBiscotti@users.noreply.github.com>
2025-05-18 21:19:55 +0200

    tests for base64 encoding

---
+++ src/test/java/dk/kavv/uuideck/encoding/EightBitBase64EncoderTest.java
@@ -0,0 +1,27 @@
+package dk.kavv.uuideck.encoding;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class EightBitBase64EncoderTest {
+    private final EightBitBase64Encoder encoder = new EightBitBase64Encoder();
+
+    @Test
+    void encode() {
+        byte[] bytes = new byte[]{
+                47, 24, 25, 36, 15, 43, 51, 9, 23, 5, 34, 18, 48, 10, 31, 30, 39, 41, 42, 20, 37, 46, 1, 12, 7, 13, 38, 40, 19, 14, 6, 8, 32, 3, 0, 4, 27, 17, 50, 33, 16, 49, 26, 45, 21, 35, 22, 11, 2, 29, 28, 44};
+        String actual = encoder.encode(bytes);
+        assertEquals("LxgZJA8rMwkXBSISMAofHicpKhQlLgEMBw0mKBMOBgggAwAEGxEyIRAxGi0VIxYLAh0cLA==", actual);
+    }
+
+    @Test
+    void decode() {
+        String in = "LxgZJA8rMwkXBSISMAofHicpKhQlLgEMBw0mKBMOBgggAwAEGxEyIRAxGi0VIxYLAh0cLA==";
+        byte[] expected = new byte[]{
+                47, 24, 25, 36, 15, 43, 51, 9, 23, 5, 34, 18, 48, 10, 31, 30, 39, 41, 42, 20, 37, 46, 1, 12, 7, 13, 38, 40, 19, 14, 6, 8, 32, 3, 0, 4, 27, 17, 50, 33, 16, 49, 26, 45, 21, 35, 22, 11, 2, 29, 28, 44};
+        byte[] actual = encoder.decode(in);
+        assertArrayEquals(expected, actual);
+    }
+}
