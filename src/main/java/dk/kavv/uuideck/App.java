ce45513e2502303e1b6510154513cf22bc176b2d
MedicodiBiscotti <13235260+MedicodiBiscotti@users.noreply.github.com>
2025-05-19 20:09:52 +0200

    picocli main

--- src/main/java/dk/kavv/uuideck/App.java
+++ src/main/java/dk/kavv/uuideck/App.java
@@ -4,12 +4,22 @@ import dk.kavv.uuideck.decks.DeckGenerator;
 import dk.kavv.uuideck.decks.RunningIntegerDeck;
 import dk.kavv.uuideck.encoding.EightBitBase64Encoder;
 import dk.kavv.uuideck.encoding.Encoder;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
 
 import java.util.Arrays;
 import java.util.Random;
+import java.util.concurrent.Callable;
 
-public class App {
+@Command(name = "uuideck", mixinStandardHelpOptions = true)
+public class App implements Callable<Integer> {
     public static void main(String[] args) {
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
+    }
+
+    @Override
+    public Integer call() throws Exception {
         DeckGenerator deckGenerator = new RunningIntegerDeck();
         Encoder encoder = new EightBitBase64Encoder();
 
@@ -19,5 +29,7 @@ public class App {
 
         String encoded = encoder.encode(deck);
         System.out.println(encoded);
+
+        return CommandLine.ExitCode.OK;
     }
 }
