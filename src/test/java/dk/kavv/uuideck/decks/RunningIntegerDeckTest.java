66817043aae8522a5c9ad722142ca4f180fe5fc5
MedicodiBiscotti <13235260+MedicodiBiscotti@users.noreply.github.com>
2025-05-21 20:26:38 +0200

    refactor array instantiation

--- src/test/java/dk/kavv/uuideck/decks/RunningIntegerDeckTest.java
+++ src/test/java/dk/kavv/uuideck/decks/RunningIntegerDeckTest.java
@@ -13,8 +13,7 @@ class RunningIntegerDeckTest {
     @Test
     void generateWithSeedReturnsDeterminedDeck() {
         byte[] deck = deckGenerator.generate(new Random(0));
-        byte[] expected = new byte[]{
-                47, 24, 25, 36, 15, 43, 51, 9, 23, 5, 34, 18, 48, 10, 31, 30, 39, 41, 42, 20, 37, 46, 1, 12, 7, 13, 38, 40, 19, 14, 6, 8, 32, 3, 0, 4, 27, 17, 50, 33, 16, 49, 26, 45, 21, 35, 22, 11, 2, 29, 28, 44};
+        byte[] expected = {47, 24, 25, 36, 15, 43, 51, 9, 23, 5, 34, 18, 48, 10, 31, 30, 39, 41, 42, 20, 37, 46, 1, 12, 7, 13, 38, 40, 19, 14, 6, 8, 32, 3, 0, 4, 27, 17, 50, 33, 16, 49, 26, 45, 21, 35, 22, 11, 2, 29, 28, 44};
         assertArrayEquals(expected, deck);
     }
 
