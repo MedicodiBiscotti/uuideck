ea457c8ef675f55137b3c1b7848e3d6315145e4b
MedicodiBiscotti <13235260+MedicodiBiscotti@users.noreply.github.com>
2025-06-21 15:56:41 +0200

    refactor and cache

--- src/main/java/dk/kavv/uuideck/utils/FactoradicUtils.java
+++ src/main/java/dk/kavv/uuideck/utils/FactoradicUtils.java
@@ -1,9 +1,13 @@
 package dk.kavv.uuideck.utils;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FactoradicUtils {
     public static final int DECK_SIZE = 52;
+    // Can also prepopulate entire cache in static block or instantiation.
+    public static final ArrayList<BigInteger> FACTORIAL_CACHE = new ArrayList<>(List.of(BigInteger.ONE));
 
     private FactoradicUtils() {
     }
@@ -12,19 +16,19 @@ public class FactoradicUtils {
         if (num < 0) {
             throw new NegativeInputException(num);
         }
-        // 0 and 1 return 1 without looping
-        BigInteger res = BigInteger.ONE;
-        while (num > 1) {
-            res = res.multiply(BigInteger.valueOf(num--));
+        while (FACTORIAL_CACHE.size() <= num) {
+            int i = FACTORIAL_CACHE.size();
+            // Start from highest, cache next value
+            FACTORIAL_CACHE.add(FACTORIAL_CACHE.get(i - 1).multiply(BigInteger.valueOf(i)));
         }
-        return res;
+        return FACTORIAL_CACHE.get(num);
     }
 
     // Takes and returns byte[] for convenience, so it can be encoded as data without having to convert an entire array.
     public static BigInteger factoradicToDecimal(byte[] factoradic) {
-        // Assumes trailing 0 in factoradic.
+        // Includes trailing 0 in factoradic because of validation.
         BigInteger res = BigInteger.ZERO;
-        for (int i = 0; i < factoradic.length - 1; i++) {
+        for (int i = 0; i < factoradic.length; i++) {
             byte val = factoradic[i];
             int radix = factoradic.length - i;
             if (val >= radix) {
@@ -32,7 +36,7 @@ public class FactoradicUtils {
             } else if (val < 0) {
                 throw new NegativeInputException(val);
             }
-            res = res.add(factorial(radix - 1).multiply(BigInteger.valueOf(val)));
+            res = res.add(BigInteger.valueOf(val).multiply(factorial(radix - 1)));
         }
         return res;
     }
