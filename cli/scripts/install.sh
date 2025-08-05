ccf8cca765ef34faef2fb4f15308990afe24f404
MedicodiBiscotti <13235260+MedicodiBiscotti@users.noreply.github.com>
2025-08-05 15:52:16 +0200

    fix install script, build timestamp, and workflow

--- cli/scripts/install.sh
+++ cli/scripts/install.sh
@@ -1,13 +1,28 @@
 #!/usr/bin/env bash
 
+# Possible improvements
+# 1. Make wrapper executable instead and put in ~/.local/bin
+# 2. Put completion in bash-completion dir instead of eager sourcing.
+# 3. If none of those, check XDG for rc location.
+
 echo "Copying artifacts"
-[ -d ~/uuideck ] || mkdir ~/uuideck
+mkdir -p ~/uuideck
 cp -f target/{uuideck.jar,uuideck_completion} ~/uuideck
 
-echo "Adding wrapper function (if needed)"
-grep uuideck --quiet ~/.bashrc || cat << 'EOF' >> ~/.bashrc
+append_wrapper_function () {
+  echo "Adding wrapper function (if needed) to $1"
+  grep uuideck --quiet "$1" || cat << 'EOF' >> "$1"
 uuideck () {
   java -jar ~/uuideck/uuideck.jar "$@"
 }
 . ~/uuideck/uuideck_completion
 EOF
+}
+
+if [ -e ~/.bashrc ]; then
+  append_wrapper_function ~/.bashrc
+fi
+
+if [ -e ~/.zshrc ]; then
+  append_wrapper_function ~/.zshrc
+fi
--- core/src/main/java/dk/kavv/uuideck/errorhandling/ShortException.java
+++ core/src/main/java/dk/kavv/uuideck/errorhandling/ShortException.java
@@ -2,6 +2,7 @@ package dk.kavv.uuideck.errorhandling;
 
 /**
  * Marker interface for not printing the stacktrace when the exception is propagated up to error handler.
+ * Not great that this violates separation of concerns, but it was an easy, scalable solution.
  */
 public interface ShortException {
 }
