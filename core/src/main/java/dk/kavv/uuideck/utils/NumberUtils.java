218429f974a3e67f4845db76aaf660b43720c7da
MedicodiBiscotti <13235260+MedicodiBiscotti@users.noreply.github.com>
2025-12-07 17:33:57 +0100

    number to text for single digits

--- core/src/main/java/dk/kavv/uuideck/utils/NumberUtils.java
+++ core/src/main/java/dk/kavv/uuideck/utils/NumberUtils.java
@@ -4,11 +4,135 @@ import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class NumberUtils {
+
+    public static final String[] ONES = {"", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+    public static final String[] TEN_TO_NINETEEN = {"ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
+    public static final String[] TENS = {"", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
+    public static final String HUNDRED = "hundred";
+    public static final String[] THOUSANDS = {"thousand",
+            "million",
+            "billion",
+            "trillion",
+            "quadrillion",
+            "quintillion",
+            "sextillion",
+            "septillion",
+            "octillion",
+            "nonillion",
+            "decillion",
+            "undecillion",
+            "duodecillion",
+            "tredecillion",
+            "quattuordecillion",
+            "quindecillion",
+            "sexdecillion",
+            "septendecillion",
+            "octodecillion",
+            "novemdecillion",
+            "vigintillion",
+            "unvigintillion",
+            "duovigintillion",
+            "trevigintillion",
+            "quattuorvigintillion",
+            "quinvigintillion",
+            "sexvigintillion",
+            "septenvigintillion",
+            "octovigintillion",
+            "novemvigintillion",
+            "trigintillion",
+            "untrigintillion",
+            "duotrigintillion",
+            "tretrigintillion",
+            "quattuortrigintillion",
+            "quintrigintillion",
+            "sextrigintillion",
+            "septentrigintillion",
+            "octotrigintillion",
+            "novemtrigintillion",
+            "quadragintillion",
+            "unquadragintillion",
+            "duoquadragintillion",
+            "trequadragintillion",
+            "quattuorquadragintillion",
+            "quinquadragintillion",
+            "sexquadragintillion",
+            "septenquadragintillion",
+            "octoquadragintillion",
+            "novemquadragintillion",
+            "quinquagintillion",
+            "unquinquagintillion",
+            "duoquinquagintillion",
+            "trequinquagintillion",
+            "quattuorquinquagintillion",
+            "quinquinquagintillion",
+            "sexquinquagintillion",
+            "septenquinquagintillion",
+            "octoquinquagintillion",
+            "novemquinquagintillion",
+            "sexagintillion",
+            "unsexagintillion",
+            "duosexagintillion",
+            "tresexagintillion",
+            "quattuorsexagintillion",
+            "quinsexagintillion",
+            "sexsexagintillion",
+            "septensexagintillion",
+            "octosexagintillion",
+            "novemsexagintillion",
+            "septuagintillion",
+            "unseptuagintillion",
+            "duoseptuagintillion",
+            "treseptuagintillion",
+            "quattuorseptuagintillion",
+            "quinseptuagintillion",
+            "sexseptuagintillion",
+            "septenseptuagintillion",
+            "octoseptuagintillion",
+            "novemseptuagintillion",
+            "octogintillion",
+            "unoctogintillion",
+            "duooctogintillion",
+            "treoctogintillion",
+            "quattuoroctogintillion",
+            "quinoctogintillion",
+            "sexoctogintillion",
+            "septenoctogintillion",
+            "octooctogintillion",
+            "novemoctogintillion",
+            "nonagintillion",
+            "unnonagintillion",
+            "duononagintillion",
+            "trenonagintillion",
+            "quattuornonagintillion",
+            "quinnonagintillion",
+            "sexnonagintillion",
+            "septennonagintillion",
+            "octononagintillion",
+            "novemnonagintillion",
+            "centillion",
+            "uncentillion",
+            "duocentillion",
+    };
+
+    /**
+     * Spells out a number in text.
+     *
+     * @param number Number to spell out.
+     * @return Textual representation of the number.
+     */
+    /*
+    https://en.wikipedia.org/wiki/Names_of_large_numbers
+    https://simple.wikipedia.org/wiki/Names_of_large_numbers
+    Can use the Conway–Guy system to create names for arbitrarily large numbers beyond 10^309 of my static list. More difficult to implement.
+    10^309 should be enough for 171! (factorial).
+    You can verify results with https://www.calculatorsoup.com/calculators/conversions/numberstowords.php.
+     */
     public static String toText(Number number) {
+        if (number.intValue() == 0) return "zero";
         String s = number.toString();
-        int len = s.length();
         // I'd rather pad once than check for length every time.
-        return padToGroupOfThree(number);
+        String padded = padToGroupOfThree(number);
+        return ONES[number.intValue()];
     }
 
     /**
--- core/src/test/java/dk/kavv/uuideck/utils/NumberUtilsTest.java
+++ core/src/test/java/dk/kavv/uuideck/utils/NumberUtilsTest.java
@@ -1,6 +1,8 @@
 package dk.kavv.uuideck.utils;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -40,5 +42,13 @@ class NumberUtilsTest {
     @Test
     void given100000WhenPadToGroupOfThreeThenPad0() {
         assertEquals("100000", NumberUtils.padToGroupOfThree(100_000));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"0,zero", "1,one", "2,two", "3,three", "4,four", "5,five", "6,six", "7,seven", "8,eight", "9,nine",
+            "1001,one thousand one", "123456789,one hundred twenty three million four hundred fifty six thousand seven hundred eighty nine"
+    })
+    void givenNumberWhenToTextReturnName(long input, String expected) {
+        assertEquals(expected, NumberUtils.toText(input));
     }
 }
